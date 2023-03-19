b5f78b2948980ff37f33f5df19d9d999585893d6
changfeng <changfeng@gaoding.com>
2023-03-19 17:38:16 +0800

    fix: disable @typescript-eslint/no-var-requires for .cjs

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -115,7 +115,7 @@ module.exports = {
             },
         },
         {
-            files: ['*.js'],
+            files: ['*.js', '*.cjs'],
             rules: {
                 '@typescript-eslint/no-var-requires': off,
             },
