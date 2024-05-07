6c5496c47fb56623e63b4dee3af117ebbe2f4702
余腾靖 <ytj2713151713@gmail.com>
2024-05-08 00:25:24 +0800

    fix: typescript-eslint/no-unused-vars report js files

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -39,50 +39,51 @@ module.exports = defineConfig({
                 'jsdoc/require-returns-check': off,
                 'jsdoc/require-returns-type': off,
 
-                // TS
-                '@typescript-eslint/array-type': [error, { default: 'array-simple' }],
-                '@typescript-eslint/ban-ts-comment': [
-                    error,
-                    { 'ts-ignore': 'allow-with-description' },
-                ],
+                // seems will cause error whe lint json if put in rules
                 '@typescript-eslint/consistent-type-imports': [
                     error,
                     { prefer: 'type-imports', disallowTypeAnnotations: false },
                 ],
-                '@typescript-eslint/consistent-type-definitions': [error, 'interface'],
-                '@typescript-eslint/prefer-ts-expect-error': error,
-
-                // Override JS
-                '@typescript-eslint/no-unused-vars': off,
-                '@typescript-eslint/no-redeclare': error,
-                '@typescript-eslint/no-use-before-define': [
-                    error,
-                    { functions: false, classes: false, variables: true },
-                ],
-                '@typescript-eslint/no-dupe-class-members': error,
-                '@typescript-eslint/no-loss-of-precision': error,
-                '@typescript-eslint/lines-between-class-members': [
-                    error,
-                    'always',
-                    { exceptAfterSingleLine: true },
-                ],
-
-                // off
-                '@typescript-eslint/ban-ts-ignore': off,
-                '@typescript-eslint/ban-types': off,
-                '@typescript-eslint/consistent-indexed-object-style': off,
-                '@typescript-eslint/explicit-function-return-type': off,
-                '@typescript-eslint/explicit-member-accessibility': off,
-                '@typescript-eslint/explicit-module-boundary-types': off,
-                '@typescript-eslint/naming-convention': off,
-                '@typescript-eslint/no-empty-function': off,
-                '@typescript-eslint/no-empty-interface': off,
-                '@typescript-eslint/no-explicit-any': off,
-                '@typescript-eslint/no-namespace': off,
-                '@typescript-eslint/no-non-null-assertion': off,
-                '@typescript-eslint/parameter-properties': off,
-                '@typescript-eslint/triple-slash-reference': off,
             },
         },
     ],
+
+    rules: {
+        // TS
+        '@typescript-eslint/array-type': [error, { default: 'array-simple' }],
+        '@typescript-eslint/ban-ts-comment': [error, { 'ts-ignore': 'allow-with-description' }],
+        '@typescript-eslint/consistent-type-definitions': [error, 'interface'],
+        '@typescript-eslint/prefer-ts-expect-error': error,
+
+        // Override JS
+        '@typescript-eslint/no-unused-vars': off,
+        '@typescript-eslint/no-redeclare': error,
+        '@typescript-eslint/no-use-before-define': [
+            error,
+            { functions: false, classes: false, variables: true },
+        ],
+        '@typescript-eslint/no-dupe-class-members': error,
+        '@typescript-eslint/no-loss-of-precision': error,
+        '@typescript-eslint/lines-between-class-members': [
+            error,
+            'always',
+            { exceptAfterSingleLine: true },
+        ],
+
+        // off
+        '@typescript-eslint/ban-ts-ignore': off,
+        '@typescript-eslint/ban-types': off,
+        '@typescript-eslint/consistent-indexed-object-style': off,
+        '@typescript-eslint/explicit-function-return-type': off,
+        '@typescript-eslint/explicit-member-accessibility': off,
+        '@typescript-eslint/explicit-module-boundary-types': off,
+        '@typescript-eslint/naming-convention': off,
+        '@typescript-eslint/no-empty-function': off,
+        '@typescript-eslint/no-empty-interface': off,
+        '@typescript-eslint/no-explicit-any': off,
+        '@typescript-eslint/no-namespace': off,
+        '@typescript-eslint/no-non-null-assertion': off,
+        '@typescript-eslint/parameter-properties': off,
+        '@typescript-eslint/triple-slash-reference': off,
+    },
 });
